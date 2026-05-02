90f5dac0377dfd0ff25759547c1d96ae881e5824
guilhermeivo <guilhermeivob@gmail.com>
2026-05-02 18:00:34 -0300

    feat: php: add docker configuration

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+cat > /etc/msmtprc <<EOF
+defaults
+auth           on
+tls            on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+logfile        /var/log/msmtp.log
+
+account gmail
+host ${SMTP_HOST}
+port ${SMTP_PORT}
+from ${SMTP_FROM}
+user ${SMTP_USER}
+password ${SMTP_PASS}
+
+account default : gmail
+EOF
+
+chown $USER:www-data /etc/msmtprc
+chmod 640 /etc/msmtprc
+
+chown -R www-data:www-data /var/www/html/conf
+
+exec php-fpm
